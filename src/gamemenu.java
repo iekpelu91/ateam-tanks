62ed425842cc7a626de987863498b59d17afc109
iekpelu91 <iekpelu@slu.edu>
2014-03-27 21:49:54 -0500

    buttons....push em

--- src/gamemenu.java
+++ src/gamemenu.java
@@ -14,17 +14,25 @@ public class gamemenu extends JPanel implements ActionListener {
 	final JButton down = new JButton("DOWN");
 	final JButton left = new JButton("LEFT");
 	final JButton right = new JButton("RIGHT");
+	final JButton newgame = new JButton("New Game");
+	final JButton shoot = new JButton("Fire");
+	final JButton wait = new JButton("Wait");
+	final JButton turn = new JButton("Turn");
 	
 	private GameBoard game = null;
 
 	public ButtonPanel(GameBoard g) {
 		game = g;
+		add(newgame);
 		add(instructions);
 		//add(restart);
 		add(up);
 		add(down);
 		add(left);
 		add(right);
+		add(shoot);
+		add(wait);
+		add(turn);
 		setLayout(new GridLayout(0, 6));
 	}
 
